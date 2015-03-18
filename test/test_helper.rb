7e7d31cbfa00245226ea38abb2bda3f23e071d28
Dave Woodall <dave.woodall@livingsocial.com>
2015-03-18 12:20:49 -0600

    Attempting basic setup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,11 @@
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require "minitest/unit"
+require "mocha/mini_test"
+require 'mocha/mini_test'
+require 'capybara/rails'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
@@ -8,8 +13,3 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
-
-require "minitest/unit"
-require "mocha/mini_test"
-require 'mocha/mini_test'
-require 'capybara/rails'

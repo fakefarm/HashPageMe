c6c2aba4b754e749fd6390106d923f2f20176001
Dave Woodall <dave.woodall@livingsocial.com>
2015-04-23 16:52:50 -0600

    Trying to use VCR but it’s not working #TDD

--- test/features/getting_tweets_test.rb
+++ test/features/getting_tweets_test.rb
@@ -11,10 +11,12 @@ end
 
 feature 'getting the tweets' do
   should 'get to a show page' do
-    visit root_path
-    page.fill_in 'fetcher_username', :with => 'catmathdo'
-    click_button "get tweets"
-    page.must_have_content "catmathdo has 71 tweets!"
-    # _dw Question - how do I mock out this call to twitter? Or, is the point not to?
+    VCR.use_cassette("catmathdo_handle") do
+      visit root_path
+      page.fill_in 'fetcher_username', :with => 'catmathdo'
+      click_button "get tweets"
+      page.must_have_content "catmathdo has 71 tweets!"
+    end
+    # _dw Question - how do I mock out this call to twitter? Or, is the point not to? Why isn't VCR working?
   end
 end

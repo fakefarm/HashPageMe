fbf78a8ec64e53898d2be8fbc8eb822347704ae6
Dave Woodall <dave.woodall@livingsocial.com>
2015-07-08 15:07:59 -0600

    Write tests for About_Keyword

--- test/models/about_keyword_test.rb
+++ test/models/about_keyword_test.rb
@@ -1,18 +1,38 @@
 require 'test_helper'
 
-class AboutKeyword < ActiveSupport::TestCase
-  context "#for_user" do
-    should 'return all tweets with #aboutMe or #about hashtag' do
-      # User.any_instance.stubs(:about_tweets).returns(raw_tweets(:about))
+class AboutKeywordTest < ActiveSupport::TestCase
+  context "self.for_user" do
 
-      # RawTweet.stubs(:where).returns(raw_tweets(:about))
-      # about_tweets = AboutKeyword.new('dave')
-      # assert_equal 1, about_tweets
+    should 'return an empty array if user has no tweets with hashtag' do
+      assert_equal AboutKeyword.for_user('fake_user'), []
+    end
 
-      RawTweet.stub :where, raw_tweets(:about) do   # stub goes away once the block is done
-        about_tweets = AboutKeyword.new('dave')
-        assert_equal 1, about_tweets
-      end
+    should 'return all tweets with about hashtag' do
+      tweet = raw_tweets(:about)
+      about_tweet = AboutKeyword.for_user(tweet.username)
+      assert_equal 1, about_tweet.count
+    end
+
+    should 'return all tweets with aboutme hashtag' do
+      tweet = raw_tweets(:aboutme)
+      about_tweet = AboutKeyword.for_user(tweet.username)
+      assert_equal 1, about_tweet.count
     end
   end
 end
+
+## Jon Daniel Notes
+
+# PREFER OBJECTS OVER STRING
+# code smell if you're passing in a string over passing an object.
+# example:  Foo.user('dave')
+# it's okay if it's string parsing kind of work
+# but if it's not, better to pass a user object.
+
+## Exceptions
+# should occur when something happens that means that the rest of this processing
+# from here on out can't go on.
+# example
+# user_profile(user)
+# if username doesn't exist - Exception: record not found. (AR::RecordNotFound)
+# working with exteral services - you will encounter an exception.
